f43f41156cda28b7497c7fd5f123d8bb9aec17e6
Karolis <kikarolis@gmail.com>
2022-03-08 20:21:24 +0000

    Changed post_id to topic_id

--- database/crud_comment_data.py
+++ database/crud_comment_data.py
@@ -2,16 +2,16 @@ import psycopg2
 from database import connect_to_database
 
 
-def insert_entry(post_id, post, table_name):
+def insert_entry(topic_id, post, table_name):
     try:
         # Connect to the database
         connection = connect_to_database.connect_to_the_database()
         cursor = connection.cursor()
 
-        postgres_insert_query = """ INSERT INTO """ + table_name + """(post_id, comment_id, forum_username, forum_profile_url, 
+        postgres_insert_query = """ INSERT INTO """ + table_name + """(topic_id, comment_id, forum_username, forum_profile_url, 
         telegram_username, campaigns, post_time) VALUES (%s, %s, %s, %s, %s, %s, %s)"""
 
-        record_to_insert = (post_id, post[0], str(post[1]), str(post[2]), str(post[3]), str(post[4]), str(post[5]))
+        record_to_insert = (topic_id, post[0], str(post[1]), str(post[2]), str(post[3]), str(post[4]), str(post[5]))
 
         cursor.execute(postgres_insert_query, record_to_insert)
         connection.commit()
