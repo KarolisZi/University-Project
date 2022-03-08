f2d827cc1d42d1ff651295fad4d35665daf93fe0
Karolis <kikarolis@gmail.com>
2022-03-08 20:20:11 +0000

    Removed some redundant arguments in function calling

--- control/control.py
+++ control/control.py
@@ -39,18 +39,18 @@ def create_all_post_records_in_database(database_table_name, url):
         print("Inserting %s posts into table: %s" % (len(posts_per_page), database_table_name))
 
 
-def populate_database_main_page(check, database_table_name, url):
+def populate_database_main_page(check):
     # Create database table
-    create_table.create_home_page_database(database_table_name)
+    create_table.create_home_page_database()
 
     # if check True populate the database with the given page
     # if check False populate the database with all pages
     match check:
         case "one":
 
-            create_one_page_post_records_in_database(database_table_name, url)
+            create_one_page_post_records_in_database(constant.TABLE_NAME_HOME_PAGE, constant.FIRST_PAGE_URL)
         case "all":
-            create_all_post_records_in_database(database_table_name, url)
+            create_all_post_records_in_database(constant.TABLE_NAME_HOME_PAGE, constant.FIRST_PAGE_URL)
 
 
 """
@@ -71,6 +71,7 @@ def create_one_post_one_page_comment_records_in_database(data, database_table_na
 
 
 def create_one_post__all_pages_comment_records_in_database(data, database_table_name):
+
     # Get the number of the last comment page
     page_id = scrape_forum_post_comments.fetch_last_comment_page_id(data[0][1])
 
@@ -109,17 +110,21 @@ def create_all_post_all_pages_comment_records_in_database(data, database_table_n
             print("Inserting %s comments into table: %s" % (len(comments), database_table_name))
 
 
-def populate_database_comment_page(check, database_table_name):
+def populate_database_comment_page(check):
+    """
+
+    :rtype: object
+    """
     # Fetch all URLs and IDs of the comments
     data = crud_home_page_data.fetch_all_id_url(constant.TABLE_NAME_HOME_PAGE)
 
     # Create database table for comments
-    create_table.create_comments_database(database_table_name)
+    create_table.create_comments_database()
 
     match check:
         case "one_one":
-            create_one_post_one_page_comment_records_in_database(data, database_table_name)
+            create_one_post_one_page_comment_records_in_database(data, constant.TABLE_NAME_COMMENT_PAGE)
         case "one_all":
-            create_one_post__all_pages_comment_records_in_database(data, database_table_name)
+            create_one_post__all_pages_comment_records_in_database(data, constant.TABLE_NAME_COMMENT_PAGE)
         case "all_all":
-            create_all_post_all_pages_comment_records_in_database(data, database_table_name)
+            create_all_post_all_pages_comment_records_in_database(data, constant.TABLE_NAME_COMMENT_PAGE)
