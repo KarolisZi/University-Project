bfdc1e5f7fedd2c3fcc5c8da015aa2d7bb0df9b6
Karolis <kikarolis@gmail.com>
2022-03-22 10:43:29 +0000

    Added reverse option to read the comments from the last page, moved the insertion loop to the crud_comment_data class

--- control/control.py
+++ control/control.py
@@ -7,19 +7,20 @@ from values import constant
 
 """
 ================================================================================================
-CONTROL OPERATIONS FOR THE MAIN PAGE DATA
+CONTROL OPERATIONS FOR THE MAIN PAGE DATA (TOPICS):
+    @create_one_page_post_records_in_database(): scrape the first page of the forum
+    @create_all_post_records_in_database(): scrape all pages of the forum
+    @populate_database_main_page(): create database, choose how many pages to scrape
 ================================================================================================
 """
 
 
 # Create records of every post in the given URL that includes [BOUNTY] in the name
-def create_one_page_post_records_in_database(database_table_name, url):
-    posts_per_page = scrape_forum_home_page.fetch_post_from_url(url)
+def create_one_page_post_records_in_database(url):
 
-    for post in posts_per_page:
-        crud_home_page_data.insert_entry(post, database_table_name)
+    topics = scrape_forum_home_page.fetch_post_from_url(url)
 
-    print("Inserting %s posts into table: %s" % (len(posts_per_page), database_table_name))
+    crud_home_page_data.insert_entry(topics)
 
 
 # Create records of every post that includes [BOUNTY] in the name
@@ -31,12 +32,9 @@ def create_all_post_records_in_database(database_table_name, url):
     # Fetch every post in every page that has "BOUNTY" in the name
     for link in links:
 
-        posts_per_page = scrape_forum_home_page.fetch_post_from_url(link)
+        topics = scrape_forum_home_page.fetch_post_from_url(link)
 
-        for post in posts_per_page:
-            crud_home_page_data.insert_entry(post, database_table_name)
-
-        print("Inserting %s posts into table: %s" % (len(posts_per_page), database_table_name))
+        crud_home_page_data.insert_entry(topics)
 
 
 def populate_database_main_page(check):
@@ -48,57 +46,69 @@ def populate_database_main_page(check):
     match check:
         case "one":
 
-            create_one_page_post_records_in_database(constant.TABLE_NAME_HOME_PAGE, constant.FIRST_PAGE_URL)
+            create_one_page_post_records_in_database(constant.FIRST_PAGE_URL)
         case "all":
-            create_all_post_records_in_database(constant.TABLE_NAME_HOME_PAGE, constant.FIRST_PAGE_URL)
+            create_all_post_records_in_database(constant.FIRST_PAGE_URL)
 
 
 """
 ================================================================================================
 CONTROL OPERATIONS FOR THE COMMENT PAGE DATA
+    @create_first_database_post_one_page_comment_records_in_database():  populate the database with comments from first page
+    @create_first_database_post__all_pages_comment_records_in_database(): populate the database with all comments from one topic
+    @create_all_post_first_page_comment_records_in_database(): populate the database with the first page of every topic
+    @create_all_post_all_pages_comment_records_in_database(): populate the database with all comments form all posts
+    @populate_database_comment_page(): control how many comment pages to insert into the database
+        @reverse - start reading comments from the last page
 ================================================================================================
 """
 
 
-def create_first_database_post_one_page_comment_records_in_database(data, database_table_name):
-    comments = scrape_forum_post_comments.fetch_comments_from_url(data[0][1], data[0][2])
+def create_first_database_post_one_page_comment_records_in_database(data, reverse):
+    if reverse:
+        page_id = scrape_forum_post_comments.fetch_last_comment_page_id(data[0][1])
+        links = scrape_forum_post_comments.generate_all_comment_page_links(page_id)
+        comments = scrape_forum_post_comments.fetch_comments_from_url(links[-1], data[0][2])
+    else:
+        comments = scrape_forum_post_comments.fetch_comments_from_url(data[0][1], data[0][2])
 
-    for comment in comments:
-        crud_comment_data.insert_entry(data[0][0], comment, database_table_name)
-
-    print("Inserting %s comments into table: %s" % (len(comments), database_table_name))
+    crud_comment_data.insert_proof_comments(data[0][0], comments[0])
+    crud_comment_data.insert_participation_comments(data[0][0], comments[1])
 
 
-def create_first_database_post__all_pages_comment_records_in_database(data, database_table_name):
+def create_first_database_post__all_pages_comment_records_in_database(data, reverse):
     # Get the number of the last comment page
     page_id = scrape_forum_post_comments.fetch_last_comment_page_id(data[0][1])
 
     # Generate all page numbers
     links = scrape_forum_post_comments.generate_all_comment_page_links(page_id)
 
+    if reverse:
+        links = list(reversed(links))
+
     # Go through all comments in all pages and store them in the database
-
     for link in links:
-
         comments = scrape_forum_post_comments.fetch_comments_from_url(link, data[0][2])
 
-        for comment in comments:
-            crud_comment_data.insert_entry(data[0][0], comment, database_table_name)
-
-        print("Inserting %s comments into table: %s" % (len(comments), database_table_name))
+        crud_comment_data.insert_proof_comments(data[0][0], comments[0])
+        crud_comment_data.insert_participation_comments(data[0][0], comments[1])
 
 
-def create_all_post_first_page_comment_records_in_database(data, database_table_name):
+def create_all_post_first_page_comment_records_in_database(data, reverse):
     for entry in data:
-        comments = scrape_forum_post_comments.fetch_comments_from_url(entry[1], entry[2])
 
-        for comment in comments:
-            crud_comment_data.insert_entry(entry[0], comment, database_table_name)
+        if reverse:
+            page_id = scrape_forum_post_comments.fetch_last_comment_page_id(entry[1])
+            links = scrape_forum_post_comments.generate_all_comment_page_links(page_id)
+            comments = scrape_forum_post_comments.fetch_comments_from_url(links[-1], entry[2])
+        else:
+            comments = scrape_forum_post_comments.fetch_comments_from_url(entry[1], entry[2])
 
-        print("Inserting %s comments into table: %s" % (len(comments), database_table_name))
+        crud_comment_data.insert_proof_comments(entry[0], comments[0])
+        crud_comment_data.insert_participation_comments(data[0][0], comments[1])
 
 
-def create_all_post_all_pages_comment_records_in_database(data, database_table_name):
+def create_all_post_all_pages_comment_records_in_database(data, reverse):
     for entry in data:
         # Get the number of the last comment page
         page_id = scrape_forum_post_comments.fetch_last_comment_page_id(entry[1])
@@ -106,30 +116,32 @@ def create_all_post_all_pages_comment_records_in_database(data, database_table_n
         # Generate all page numbers
         links = scrape_forum_post_comments.generate_all_comment_page_links(page_id)
 
+        if reverse:
+            links = list(reversed(links))
+
         # Go through all comments in all pages and store them in the database
         for link in links:
             comments = scrape_forum_post_comments.fetch_comments_from_url(link, entry[2])
 
-            print("Inserting %s comments into table: %s" % (len(comments), database_table_name))
+            crud_comment_data.insert_proof_comments(entry[0], comments[0])
+            crud_comment_data.insert_participation_comments(data[0][0], comments[1])
 
 
-def populate_database_comment_page(check):
-    """
+def populate_database_comment_page(check, reverse):
 
-    :type: object
-    """
     # Fetch all URLs and IDs of the comments
     data = crud_home_page_data.fetch_all_id_url_author(constant.TABLE_NAME_HOME_PAGE)
 
     # Create database table for comments
-    create_table.create_comments_database()
+    create_table.create_comments_proof_database()
+    create_table.create_comments_participation_database()
 
     match check:
         case "one_one":
-            create_first_database_post_one_page_comment_records_in_database(data, constant.TABLE_NAME_COMMENT_PAGE)
+            create_first_database_post_one_page_comment_records_in_database(data, reverse)
         case "one_all":
-            create_first_database_post__all_pages_comment_records_in_database(data, constant.TABLE_NAME_COMMENT_PAGE)
+            create_first_database_post__all_pages_comment_records_in_database(data, reverse)
         case "all_one":
-            create_all_post_first_page_comment_records_in_database(data, constant.TABLE_NAME_COMMENT_PAGE)
+            create_all_post_first_page_comment_records_in_database(data, reverse)
         case "all_all":
-            create_all_post_all_pages_comment_records_in_database(data, constant.TABLE_NAME_COMMENT_PAGE)
+            create_all_post_all_pages_comment_records_in_database(data, reverse)
