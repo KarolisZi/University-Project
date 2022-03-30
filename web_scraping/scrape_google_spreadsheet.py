d56ade187325348d1f4bb374ac818f4771b36b57
Karolis <kikarolis@gmail.com>
2022-03-30 15:59:48 +0100

    Added exception handling if no sheet_id is provided

--- web_scraping/scrape_google_spreadsheet.py
+++ web_scraping/scrape_google_spreadsheet.py
@@ -2,24 +2,29 @@ import gspread
 import time
 
 
-def fetch_sheet_data(id):
+def fetch_sheet_data(sheet_id):
     sheet_data = []
 
     sa = gspread.service_account(filename='API/service_account.json')
-    sh = sa.open_by_key(id)
+    try:
+        if sheet_id != '' and sheet_id is not None:
+            sh = sa.open_by_key(sheet_id)
 
-    worksheet_list = sh.worksheets()
+            worksheet_list = sh.worksheets()
 
-    for worksheet in worksheet_list:
+            for worksheet in worksheet_list:
 
-        try:
+                try:
 
-            sheet_data.append([worksheet.title, worksheet.get_all_values()])
+                    sheet_data.append([worksheet.title, worksheet.get_all_values()])
 
-        except gspread.exceptions.APIError:
+                except gspread.exceptions.APIError:
 
-            print('Quota exceeded, sleeping for 1 minute')
-            time.sleep(60)
-            sheet_data.append([worksheet.title, worksheet.get_all_values()])
+                    print('Quota exceeded, sleeping for 1 minute')
+                    time.sleep(60)
+                    sheet_data.append([worksheet.title, worksheet.get_all_values()])
 
+    except gspread.exceptions.APIError:
+        print('Error: ' + str(gspread.exceptions.APIError))
+        # Insert into the database that the sheet no longer exists
     return sheet_data
