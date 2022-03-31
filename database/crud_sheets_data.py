bf67904848cd4d292fd747bc19f9cf669a2691c3
Karolis <kikarolis@gmail.com>
2022-03-31 13:28:43 +0100

    Added followers to the sheets database table

--- database/crud_sheets_data.py
+++ database/crud_sheets_data.py
@@ -9,10 +9,10 @@ def insert_proof_comments(sheet_id, topic_id, sheet_name, data):
         connection = connect_to_database.connect_to_the_database()
         cursor = connection.cursor()
 
-        postgres_insert_query = """ INSERT INTO """ + constant.TABLE_NAME_GOOGLE_SHEETS + """(row, sheet_id, topic_id, sheet_name, timestamp, forum_username, profile_link, social_media_username) VALUES (%s, %s, %s, %s, %s, %s, %s, %s)"""
+        postgres_insert_query = """ INSERT INTO """ + constant.TABLE_NAME_GOOGLE_SHEETS + """(row, sheet_id, topic_id, sheet_name, timestamp, forum_username, profile_link, social_media_username, followers) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)"""
 
         for row in data:
-            record_to_insert = (row[0], sheet_id, topic_id, sheet_name, row[1], row[2], row[3], row[4])
+            record_to_insert = (row[0], sheet_id, topic_id, sheet_name, row[1], row[2], row[3], row[4], row[5])
             cursor.execute(postgres_insert_query, record_to_insert)
             connection.commit()
 
