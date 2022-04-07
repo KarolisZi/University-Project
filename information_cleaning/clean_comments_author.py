b373730a2bffe1b04bc938c5e59e7e803324b36a
Karolis <kikarolis@gmail.com>
2022-04-07 21:27:45 +0100

    Improved author_comment cleaning

--- information_cleaning/clean_comments_author.py
+++ information_cleaning/clean_comments_author.py
@@ -11,50 +11,59 @@ DATA CLEANING PART FOR AUTHOR COMMENT SECTION DATA
 
 
 def clean_comments(author_comments):
-
     text_image = filter_comments(author_comments)
 
-    print(text_image[1])
+    # Clean raw text comments here:
 
+    # [raw_text, image_urls, image_check, comment_index]
     return text_image
 
 
 def filter_comments(author_comments):
-    raw_text, image_links = [], []
 
-    if len(author_comments) > 0:
+    results = []
+    raw_text, image_urls, image_check, comment_index = '', [], None, ''
 
-        if len(author_comments[0].text) > 2000:
+    if author_comments:
 
-            for comment in author_comments:
-                raw_text.append(comment.text)
-        else:
-            image_links = retrieve_author_post_image_links(author_comments)
+        for i in range(0, len(author_comments)):
 
-    return [raw_text, image_links]
+            comment_index = i+1
+
+            image_urls = retrieve_author_post_image_links(author_comments[i])
+
+            raw_text = author_comments[i]
+
+            if not image_urls:
+                # Full raw_text
+                image_check = False
+            elif image_urls:
+                image_check = True
+
+            results.append([raw_text, image_urls, image_check, comment_index])
+
+    return results
 
 
-def retrieve_author_post_image_links(author_comments):
+def retrieve_author_post_image_links(comment):
     image_urls = []
 
-    for comment in author_comments:
+    img_tags = comment.find_all('img')
 
-        img_tags = comment.find_all('img')
+    for tag in img_tags:
 
-        for tag in img_tags:
+        url = tag.get('src')
 
-            url = tag.get('src')
+        if regex.image_proxy_pattern.match(url):
+            url = url.replace('https://ip.bitcointalk.org/?u=', '')
 
-            if regex.image_proxy_pattern.match(url):
-                url = url.replace('https://ip.bitcointalk.org/?u=', '')
+            url = urllib.parse.unquote(url)
 
-                url = urllib.parse.unquote(url)
+            temp = url.split('&')
 
-                temp = url.split('&')
+            url = temp[0]
 
-                url = temp[0]
-
-                image_urls.append(url)
+            image_urls.append(url)
 
     return image_urls
 
@@ -98,13 +107,11 @@ def extract_spreadsheet_links(author_comments):
 
 
 def extract_spreadsheet_ids(author_comments):
-
     sheet_ids = []
 
     links = extract_spreadsheet_links(author_comments)
 
     for link in links:
-
         deconstructed = link.split('/')
 
         sheet_ids.append(deconstructed[5])
