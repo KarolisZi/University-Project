53d3b4d70790d9f99a25ab6a9a133a6589a2c42d
Karolis <kikarolis@gmail.com>
2022-04-07 21:26:59 +0100

    Improved TOKEN retrieval from topic

--- information_cleaning/clean_topic.py
+++ information_cleaning/clean_topic.py
@@ -1,7 +1,7 @@
+import re
 from values import regex
 from information_cleaning import helper_functions
 import enchant
-import numpy as np
 
 """
 =================================================================================================================
@@ -11,6 +11,7 @@ DATA CLEANING PART FOR HOME PAGE DATA
  @ extract_id_from_url - retrieves the topic id from the URL
  @ retrieve_last_author() - retrieves the topics last post author
  @ clean_topic() - removes redundant information from the topic title
+ @ extract_coin_name() - retrieves the TOKEN name from the topic
 ================================================================================================================
 """
 
@@ -23,11 +24,10 @@ def clean_topic_data(column):
     original_topic = column[2].find('a').text
     topic = clean_topic(original_topic)
     token_name = extract_coin_name(topic)
-    author = column[3].text.strip('\n').strip('\t').rstrip().lstrip()
-    replies = column[4].text.strip('\n').strip('\t').rstrip().lstrip()
-    views = column[5].text.strip('\n').strip('\t').rstrip().lstrip()
-    last_post_time = helper_functions.convert_time(
-        column[6].text.splitlines()[3].strip('\n').strip('\t').rstrip()).lstrip()
+    author = column[3].text.strip('\n').strip('\t').strip()
+    replies = column[4].text.strip('\n').strip('\t').strip()
+    views = column[5].text.strip('\n').strip('\t').strip()
+    last_post_time = helper_functions.convert_time(column[6].text.splitlines()[3].strip('\n').strip('\t')).strip()
     last_post_author = retrieve_last_post_author(column[6].text.splitlines()[4])
 
     return [topic_id, url, original_topic, topic, token_name, author, replies, views, last_post_time, last_post_author]
@@ -53,32 +53,29 @@ def retrieve_last_post_author(last_post_author):
 
 # Clean last topic line
 def clean_topic(topic):
-    # Regex expressions for emoji's
 
+    # The number of emojis used
+    emoji_len = len(re.findall(regex.emoji_pattern, topic))
     # Remove emoji characters from topic
-    topic = regex.emoji_pattern.sub('', topic)
+    topic = regex.emoji_pattern.sub(' ', topic)
 
     # Remove underscores
     topic = topic.replace("_", "")
 
-    # Remove [BOUNTY]
-    topic = topic.replace("[Bounty]", "")
-    topic = topic.replace("[BOUNTY]", "")
-    topic = topic.replace("[BOUNTY STAR]", "")
-    topic = topic.replace("[BOUNTY ALADDIN]", "")
-    topic = topic.replace("[BOUNTY DETECTIVE]", "")
-    topic = topic.replace("[BOUNTY OPPORTUNITY]", "")
-    topic = topic.replace("[LIVE BOUNTY]", "")
-    topic = topic.replace("[ANN]", "")
-    topic = topic.replace("[ICO]", "")
-    topic = topic.replace("ICO", "")
-    topic = topic.replace("PRE-ICO", "")
-    topic = topic.replace("[CLOSED]", "")
-    topic = topic.replace("(Bounty)", "")
-    topic = topic.replace("(BOUNTY)", "")
-    topic = topic.replace("BOUNTY", "")
-    topic = topic.replace("Bounty", "")
-    topic = topic.replace("ILO", "")
+    for string in ['Bounty Star', 'Bounty Aladdin', 'Bounty Detective', 'Bounty Opportunity', 'Live Bounty', 'Bounty',
+                   'ANN', 'ICO', 'PRE-ICO', 'Closed', 'Finished', 'Ended', 'End', 'ILO', '[', ']', '(', ')', '|', ':',
+                   '!', 'Airdrop', '&']:
+        if string in topic:
+            topic = topic.replace(string, '')
+        if string.upper() in topic:
+            topic = topic.replace(string.upper(), '')
+
+    # Replace double or triple spaces with one
+    topic = topic.replace('      ', ' ')
+    topic = topic.replace('     ', ' ')
+    topic = topic.replace('    ', ' ')
+    topic = topic.replace('   ', ' ')
+    topic = topic.replace('  ', ' ')
 
     # Remove empty spaces
     topic = topic.strip()
@@ -87,40 +84,83 @@ def clean_topic(topic):
 
 
 def extract_coin_name(topic):
-
-    fist_word = ''
-    token_name = []
+    possible_token_names = []
+    token_name = ''
 
     en_US = enchant.Dict("en_US")
     en_GB = enchant.Dict("en_GB")
 
     # Higher chance for the token name to be at the start of the string
-    words = np.flip(topic.split(' '))
+    words = topic.split(' ')
 
-    for word in words:
+    """
+        TOKEN NAME FILTERING RULES (1st rule is superior to 2nd and etc...)
+        1) If the topic has a length of 1, that is the TOKEN name
+        2) The word followed by the word 'COIN', 'TOKEN', 'TOKENS', 'STABLECOIN' is the TOKEN name
+        3) Non English words with all capital letters (3 - 10 letters) and not number followed by K, k, M, m
+        4) English Words with all capital letters (3 - 5 letters)
+        5) If no TOKEN name is found then take the first word as a TOKEN name
+    """
 
-        word = word.replace('[', '')
-        word = word.replace(']', '')
-        word = word.replace('(', '')
-        word = word.replace(')', '')
-        word = word.replace('|', '')
-        word = word.replace('.', '')
-        word = word.replace(':', '')
+    # 1)
+    if len(words) == 1:
+        token_name = words[0]
+        # print('Rule #1')
+    elif len(words) > 1:
+        for i in range(0, len(words)):
 
-        if word != '':
+            words[i] = words[i].strip()
 
-            if 'ETH' not in word and 'BTC' not in word and '$' not in word and 'NFT' not in word and 'USD' not in word and 'ERC20' not in word and 'BEP20' not in word and 'APY' not in word and 'POOL' not in word:
-
-                if regex.token_all_capitals.match(word):
-                    if not en_US.check(word) and not en_GB.check(word):
-                        token_name.append(word)
-                    elif regex.token_all_capitals_short.match(word):
-                        token_name.append(word)
-
-            fist_word = word
-
+            # Making sure that other common words that would match the criteria ar not the TOKEN name
+            if symbols_abbreviations_check(words[i]):
+                # 2)
+                if 'TOKEN' == words[i].upper() or 'TOKENS' == words[i].upper() or 'COIN' == words[i].upper() or 'STABLECOIN' == words[i].upper() and i != 0:
+                    if words[i-1] != 'of':
+                        token_name = words[i - 1]
+                        # print('Rule #2')
+                        break
+                # 3)
+                elif words[i].isupper() and 1 < len(words[i]) < 15 and not regex.number_letter_format.match(words[i]):
+                    if not en_US.check(words[i]) and not en_GB.check(words[i]):
+                        token_name = words[i]
+                        # print('Rule #3')
+                        break
+                    # 4)
+                    elif words[i].isupper() and 1 < len(words[i]) < 6:
+                        possible_token_names.append(words[i])
+                        # print('Rule #4')
+    # 5)
     if not token_name:
-        token_name.append(fist_word)
+        for i in range(0, len(words)):
+            if symbols_abbreviations_check(words[i]) and 1 < len(words[i]) < 6:
+                possible_token_names.append(words[i])
+                break
+        # print('Rule #5')
+    elif token_name:
+        possible_token_names.insert(0, token_name)
+
+    return possible_token_names
 
 
-    return token_name
+def symbols_abbreviations_check(word_to_analyse):
+
+    # Array of words that should not be equal to the TOKEN name
+    abbreviations = ['ETH', 'BTC', 'NFT', 'USD', 'ERC20', 'BEP20', 'APY', 'POOL', 'Million', 'BUSD', 'KYC']
+    # Symbols that should not be in a TOKEN name
+    symbols = ['$', '.', '*', '@', ',']
+
+    for abbreviation in abbreviations:
+        if abbreviation == word_to_analyse:
+            return False
+
+    for symbol in symbols:
+        if symbol in word_to_analyse:
+            return False
+
+    if word_to_analyse.isdigit():
+        return False
+
+    return True
+
+
+
