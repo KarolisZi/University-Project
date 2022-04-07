d1f599945feddc509a8cbb2956bbcdb0145ee668
Karolis <kikarolis@gmail.com>
2022-04-07 21:26:15 +0100

    Added functions to query comment databases

--- database/db_crud_comments.py
+++ database/db_crud_comments.py
@@ -2,6 +2,16 @@ import psycopg2
 from database import connect_to_database
 from values import constant
 
+"""
+========================================================================================================================
+CREATE (INSERT) STATEMENTS
+    @ insert_proof_comments() - inserts PROOF comments into the database table
+    @ insert_participation_comments() - inserts PARTICIPATION comments into the database table
+    @ insert_author_comments() - inserts AUTHOR comments into the database table 
+    @ insert_comments_update() - inserts topic_id which have to run comment updates
+========================================================================================================================
+"""
+
 
 def insert_proof_comments(topic_id, page_id, comments):
     try:
@@ -62,17 +72,86 @@ def insert_participation_comments(topic_id, page_id, comments):
             connection.close()
 
 
-def retrieve_participation_proof_comments_username_recurrence(table_name):
+def insert_author_comments(topic_id, data):
+    try:
+        # Connect to the database
+        connection = connect_to_database.connect_to_the_database()
+        cursor = connection.cursor()
+
+        for entry in data:
+
+            postgres_insert_query = ''
+            record_to_insert = ''
+
+            if entry[2]:
+                postgres_insert_query = """ INSERT INTO """ + constant.DB_AUTHOR + """(topic_id, comment_id, text_data, image_check, image_urls) VALUES (%s, %s, %s, %s, %s)"""
+                record_to_insert = (topic_id, entry[3], entry[0].text, True, entry[1])
+            elif not entry[2]:
+                postgres_insert_query = """ INSERT INTO """ + constant.DB_AUTHOR + """(topic_id, comment_id, text_data, image_check) VALUES (%s, %s, %s, %s)"""
+                record_to_insert = (topic_id, entry[3], entry[0].text, False)
+
+            cursor.execute(postgres_insert_query, record_to_insert)
+            connection.commit()
+
+    except (Exception, psycopg2.Error) as error:
+        print("Failed to insert record into the table: ", error)
+
+    finally:
+
+        print("%s: Success!" % constant.DB_AUTHOR)
+
+        # closing database connection.
+        if connection:
+            cursor.close()
+            connection.close()
+
+
+def insert_comments_update(topic_id, replies_old, replies_new, new_post):
+    try:
+        # Connect to the database
+        connection = connect_to_database.connect_to_the_database()
+        cursor = connection.cursor()
+
+        postgres_insert_query = """ INSERT INTO """ + constant.DB_COMMENTS_UPDATE + """(topic_id, replies_old, replies_new, new_post) VALUES (%s, %s, %s, %s)"""
+
+        record_to_insert = (topic_id, replies_old, replies_new, new_post)
+        cursor.execute(postgres_insert_query, record_to_insert)
+        connection.commit()
+
+    except (Exception, psycopg2.Error) as error:
+        print("Failed to insert record into the table", error)
+
+    finally:
+
+        print("%s: Success!" % constant.DB_PROOF)
+
+        # closing database connection.
+        if connection:
+            cursor.close()
+            connection.close()
+
+
+"""
+========================================================================================================================
+READ (SELECT) STATEMENTS
+    @ retrieve_topic_ids_comments_update() - retrieve a topic_id to check if an entry already exists in the database
+    @ fetch_all_id_image_urls() - retrieve [topic_id, image_urls, image_check]
+========================================================================================================================
+"""
+
+
+def retrieve_topic_ids_comments_update(topic_id):
+    table_name = constant.DB_COMMENTS_UPDATE
     connection = connect_to_database.connect_to_the_database()
     cursor = connection.cursor()
 
     try:
 
-        postgres_insert_query = """ SELECT forum_username, count(*) as c FROM """ + table_name + """ GROUP BY forum_username"""
+        postgres_select_query = """ SELECT topic_id FROM """ + table_name
 
-        cursor.execute(postgres_insert_query)
+        cursor.execute(postgres_select_query, (topic_id,))
 
-        result = cursor.fetchall()
+        result = cursor.fetchone()
 
         connection.commit()
 
@@ -82,6 +161,90 @@ def retrieve_participation_proof_comments_username_recurrence(table_name):
         print("Failed to retrieve data from %s: %s" % (table_name, error))
 
     finally:
+        # closing database connection.
+        if connection:
+            cursor.close()
+            connection.close()
+
+
+def fetch_all_id_image_urls():
+    try:
+        connection = connect_to_database.connect_to_the_database()
+        cursor = connection.cursor()
+
+        postgres_select_query = """SELECT topic_id, image_urls, image_check FROM """ + constant.DB_AUTHOR
+
+        cursor.execute(postgres_select_query)
+
+        result = cursor.fetchall()
+
+        connection.commit()
+
+        return result
+
+    except (Exception, psycopg2.Error) as error:
+        print("Failed to insert record into the table", error)
+
+    finally:
+        # closing database connection.
+        if connection:
+            cursor.close()
+            connection.close()
+
+
+"""
+========================================================================================================================
+UPDATE STATEMENTS
+    @ insert_image_text() - inserts image data into the database as text
+    @ update_comments_update() - update comment updates which have not been executed yet
+========================================================================================================================
+"""
+
+
+def insert_image_text(topic_id, image_data):
+
+    try:
+        connection = connect_to_database.connect_to_the_database()
+        cursor = connection.cursor()
+
+        postgres_update_query = """UPDATE """ + constant.DB_AUTHOR + """ SET image_data = %s 
+                                                                                    WHERE topic_id = %s"""
+
+        records_to_insert = (image_data, topic_id)
+
+        cursor.execute(postgres_update_query, records_to_insert)
+
+        connection.commit()
+
+    except (Exception, psycopg2.Error) as error:
+        print("Failed to insert image text: ", error)
+
+    finally:
+        # closing database connection.
+        if connection:
+            cursor.close()
+            connection.close()
+
+
+def update_comments_update(topic_id, replies_new):
+    try:
+        # Connect to the database
+        connection = connect_to_database.connect_to_the_database()
+        cursor = connection.cursor()
+
+        postgres_update_query = """UPDATE """ + constant.DB_COMMENTS_UPDATE + """ SET replies_new = %s WHERE topic_id = %s"""
+
+        record_to_insert = (replies_new, topic_id)
+        cursor.execute(postgres_update_query, record_to_insert)
+        connection.commit()
+
+    except (Exception, psycopg2.Error) as error:
+        print("Failed to insert record into the table", error)
+
+    finally:
+
+        print("%s: Success!" % constant.DB_COMMENTS_UPDATE)
+
         # closing database connection.
         if connection:
             cursor.close()
