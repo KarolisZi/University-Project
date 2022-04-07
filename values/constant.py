aafada79c5c41bf03af9d0bd63b66bfa61405eaf
Karolis <kikarolis@gmail.com>
2022-04-07 21:24:10 +0100

    Added a few additional constants

--- values/constant.py
+++ values/constant.py
@@ -1,7 +1,9 @@
 FIRST_PAGE_URL = "https://bitcointalk.org/index.php?board=238.0"
+FIRST_PAGE_URL_NO_PAGE_ID = 'https://bitcointalk.org/index.php?board=238.'
 DB_HOME = "topic"
 DB_PROOF = "comments_proof"
 DB_PARTICIPATION = "comments_participation"
 DB_SHEETS = "google_sheets"
-DB_AUTHOR = "author_comments"
+DB_AUTHOR = "comments_author_raw"
+DB_COMMENTS_UPDATE = 'comments_update'
 
