5f7465b4e26d08d4f1e78b4c8b41ab60d8e601c3
Karolis <kikarolis@gmail.com>
2022-04-23 09:29:42 +0100

    Improved comment filtering and added a possibility to choose which tables to populate

--- information_cleaning/clean_comments.py
+++ information_cleaning/clean_comments.py
@@ -1,6 +1,7 @@
 from information_cleaning import clean_comments_proof
 from information_cleaning import clean_comments_participation
 from information_cleaning import clean_comments_author
+from classes.topic import Topic
 
 """
 ========================================================================================================================
@@ -13,19 +14,24 @@ DATA CLEANING PART FOR COMMENT SECTION DATA
 """
 
 
-def clean(comments, topic_author):
+def clean(comments, topic_author, tables, topic_id):
+
+    cleaned_proof_comments, cleaned_participation_comments, cleaned_author_comments = [], [], []
+    topic = Topic()
 
     proof_participation_author = filter_comment_section_data(comments, topic_author)
 
-    cleaned_proof_comments = clean_comments_proof.clean_comments(proof_participation_author[0])
+    if 'proof' in tables:
+        cleaned_proof_comments = clean_comments_proof.clean_comments(proof_participation_author[0])
 
-    cleaned_participation_comments = clean_comments_participation.clean_comments(proof_participation_author[1])
+    if 'participation' in tables:
+        cleaned_participation_comments = clean_comments_participation.clean_comments(proof_participation_author[1])
 
-    cleaned_author_comments = clean_comments_author.clean_comments(proof_participation_author[2])
+    if 'author' in tables:
+        cleaned_author_comments = clean_comments_author.clean_comments(proof_participation_author[2], topic_id)
+        topic.set_sheet_ids(clean_comments_author.extract_spreadsheet_ids(proof_participation_author[2]))
 
-    sheet_ids = clean_comments_author.extract_spreadsheet_ids(proof_participation_author[2])
-
-    return [cleaned_proof_comments, cleaned_participation_comments, cleaned_author_comments, sheet_ids]
+    return [cleaned_proof_comments, cleaned_participation_comments, cleaned_author_comments, topic]
 
 
 def filter_comment_section_data(comments, topic_author):
@@ -38,16 +44,15 @@ def filter_comment_section_data(comments, topic_author):
         if type(poster_info) is not None and poster_info is not None:
 
             forum_username = poster_info.find('a').text
+            text = str(comment.text.upper())
 
-            if "PROOF" in str(comment.text.upper()) and forum_username != topic_author:
-
-                proof_comments.append(comment)
-
-            elif forum_username == topic_author:
-
+            if forum_username == topic_author:
                 author_comments.append(comment)
-
-            else:
+            elif "DAY" in text or "WEEK" in text and forum_username != topic_author and 'PROOF' not in text:
                 participation_comments.append(comment)
+            elif 'PROOF' in text:
+                proof_comments.append(comment)
+            elif 'ADDRESS' in text and 'TELEGRAM' in text and 'USERNAME' in text and 'FORUM' in text:
+                proof_comments.append(comment)
 
     return [proof_comments, participation_comments, author_comments]
