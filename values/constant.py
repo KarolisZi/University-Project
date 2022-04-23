50bf0e0809be9900d799028cd2a1fa23fa253138
Karolis <kikarolis@gmail.com>
2022-04-23 09:35:10 +0100

    Added relevant constants

--- values/constant.py
+++ values/constant.py
@@ -1,9 +1,22 @@
+# WEBSITE
 FIRST_PAGE_URL = "https://bitcointalk.org/index.php?board=238.0"
 FIRST_PAGE_URL_NO_PAGE_ID = 'https://bitcointalk.org/index.php?board=238.'
-DB_HOME = "topic"
+TOPIC_PAGE_URL = 'https://bitcointalk.org/index.php?topic='
+# DATABASE
+# Schema
+SCHEMA = 'data_2022_03'
+# Topic related
+DB_TOPIC = "topic"
+DB_SUCCESSFUL_TRANSFERS = 'successful_transfers'
+DB_TOPIC_REWARD_RULES = 'reward_rules'
+# Comment related
 DB_PROOF = "comments_proof"
 DB_PARTICIPATION = "comments_participation"
-DB_SHEETS = "google_sheets"
-DB_AUTHOR = "comments_author_raw"
+DB_AUTHOR = "comments_author"
 DB_COMMENTS_UPDATE = 'comments_update'
+DB_COMMENT_SCRAPE_ERRORS = 'comments_scrape_error'
+# Sheets related
+DB_SHEETS = "google_sheets"
+
+
 
