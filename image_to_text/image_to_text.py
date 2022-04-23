c88ef2044f511f30a55a6d63839263d40978356c
Karolis <kikarolis@gmail.com>
2022-04-23 09:34:45 +0100

    Scrape images, download as png and convert to text

--- image_to_text/image_to_text.py
+++ image_to_text/image_to_text.py
@@ -1,21 +1,19 @@
 import pytesseract
 from PIL import Image
+Image.LOAD_TRUNCATED_IMAGES = True
 
 absolute_path = 'C:/Users/kikar.LAPTOP-Q25785DG/OneDrive/Desktop/University-Project/images/'
-pytesseract.pytesseract.tesseract_cmd = r"C:\Users\kikar.LAPTOP-Q25785DG\AppData\Local\Programs\Tesseract-OCR\tesseract.exe"
+pytesseract.pytesseract.tesseract_cmd = r"C:\Users\kikar.LAPTOP-Q25785DG\OneDrive\Desktop\University-Project\Tesseract-OCR\tesseract.exe"
 
 
-def convert(topic_id, len_images):
-    results = []
+def convert(topic_id, image_id):
+    name = str(topic_id) + "_" + str(image_id) + ".png"
+    image_path = absolute_path + name
 
-    for i in range(0, len_images):
-        name = str(topic_id) + "_" + str(i) + ".png"
-        image_path = absolute_path + name
-        try:
-            img = Image.open(image_path)
-            text = pytesseract.image_to_string(img)
-            results.append(text)
-        except FileNotFoundError as error:
-            print('The picture %s has not been found for conversion to text: ' % name, error)
+    try:
+        img = Image.open(image_path)
+        text = pytesseract.image_to_string(img)
+    except FileNotFoundError as error:
+        print('The picture %s has not been found for conversion to text: ' % name, error)
 
-    return results
+    return text
--- web_scraping/scrape_imgur.py
+++ web_scraping/scrape_imgur.py
@@ -1,29 +1,38 @@
 import urllib.request
 from os.path import exists
+from image_to_text import image_to_text
 
 absolute_path = 'C:/Users/kikar.LAPTOP-Q25785DG/OneDrive/Desktop/University-Project/images/'
 
 
-def retrieve_imgur_image(topic_id, image_urls):
-    print("Retrieving images for topic %s" % topic_id)
-    if image_urls:
+def insert_image_data(author_comments):
 
-        image_urls = image_urls.replace('{', '')
-        image_urls = image_urls.replace('}', '')
+    author_comments_text = []
 
-        image_url = image_urls.split(',')
-        number_of_images = len(image_url)
+    for comment in author_comments:
 
-        for i in range(0, len(image_url)):
-            name = str(topic_id) + "_" + str(i) + ".png"
-            image_path = absolute_path + name
-            if not exists(image_path):
-                try:
-                    urllib.request.urlretrieve(image_url[i], image_path)
-                except Exception as error:
-                    number_of_images -= 1
-                    print("Encountered an error when retrieving image number %s for topic %s: " % (str(i+1), topic_id), error)
-        return number_of_images
-    else:
-        print("No image urls provided for topic %s" % topic_id)
-        return 0
+        topic_id, image_urls, raw_text = comment[0], comment[2], comment[3]
+
+        if image_urls:
+            number_of_images = len(image_urls)
+            for i in range(0, number_of_images):
+                no_errors = True
+                name = str(topic_id) + "_" + str(i) + ".png"
+                image_path = absolute_path + name
+                if not exists(image_path):
+                    try:
+                        urllib.request.urlretrieve(image_urls[i], image_path)
+                    except Exception as error:
+                        no_errors = False
+                        print("Encountered an error when retrieving image number: %s topic_id: %s url: %s: " % (str(i), topic_id, image_urls[i]), error)
+
+                    if no_errors:
+                        image_insert_string = ('image_insert_here: %s' % image_urls[i])
+                        image_text = image_to_text.convert(topic_id, i)
+                        raw_text = raw_text.replace(image_insert_string, image_text)
+        else:
+            print("No image urls provided for topic %s" % topic_id)
+
+        author_comments_text.append(raw_text)
+
+    return author_comments_text
